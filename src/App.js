99fa0a65b16ab3cdf407e907f26d20138a2d05bf
sayali <sayaliundale@gmail.com>
2023-12-23 16:13:14 +0530

    IntroPage Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "./Components/Header";
+import Intropage from "./Components/Intropage";
 
 
 const App =() =>{
   return(
     <>
     <Header/>
-
+    <Intropage/>
     </>
 
   );
