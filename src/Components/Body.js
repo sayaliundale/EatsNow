54c1888c2fbb74f6dcfd24eb232ef6aaebe24462
sayali <sayaliundale@gmail.com>
2023-12-26 15:57:19 +0530

    Search feature included

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,52 +1,69 @@
-import React, { useEffect, useState } from 'react'
-import "../Style/Body.css"
-import RestCard from './RestCard'
-import Shimmarui from './Shimmarui'
-
+import React, { useEffect, useState } from 'react';
+import "../Style/Body.css";
+import RestCard from './RestCard';
+import Shimmarui from './Shimmarui';
 
 const Body = () => {
   const [resofList, setRestofList] = useState([]);
+  const [filterRestaurant, setFilterRest] = useState([]);
+  //Again created it to solve the problem when u search in first you'll  get result but 
+  //second time u'll go it will not search 
+  const [searchText, setSearchtext] = useState("");
 
   useEffect(() => {
     fetchdata();
   }, []);
 
   const fetchdata = async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.8761653&lng=75.3433139&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-    );
-    const json1 = await data.json();
-    //console.log(json1.data.cards[5].card.card.gridElements.infoWithStyle.restaurants);
-    setRestofList(json1?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-    //beacuse of API structure 
-    //?. is optional chaning if json1 is present then we can go for data like this to handles errors
+    try {
+      const data = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.8761653&lng=75.3433139&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      );
+      const json1 = await data.json();
+      setRestofList(json1?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+      setFilterRest(json1?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+      //here also we'll set the all restarunt data in setFilteredRest aslo, so whenever we'will search second time only update the setFilterRest everytime so problem is solved
+
+    }
+    catch (error) {
+      console.error("Error fetching data:", error);
+    }
   };
 
-  if(resofList.length===0){
-    return <Shimmarui/>;
+  if (resofList.length === 0) {
+    return <Shimmarui />;
   }
 
   return (
     <>
       <div className="body">
         <div className="rating">
-          <button id="btn" onClick={() => {
-            let filtered = resofList.filter((res) => res.stars > 4.5);
-            setRestofList(filtered);
-          }}>
-            Top rated restaurants</button>
-        </div>
-        <div className="container">
-          {resofList.map((restaurant) => (
-            ((console.log(restaurant)), (
-              <RestCard key={restaurant.info.id} resData={restaurant} />
-            ))
-          ))}
+          <button id="btn" onClick={() => fetchdata()}>
+            Top rated restaurants
+          </button>
+
+          <p><input type="text" value={searchText}
+            onChange={(e) => setSearchtext(e.target.value)} />
+
+            <button onClick={() => {
+              const filterRest = resofList.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase()));
+              setFilterRest(filterRest);
+            }}>
+              Search</button></p>
+
         </div>
 
+        <div className="container">
+
+          {filterRestaurant.map((restaurant) => (
+            <RestCard key={restaurant.info.id} resData={restaurant} />
+          ))}
+
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
