d2d7f51db531e29be1102d6a41ddf152adfa7e9f
sayali <sayaliundale@gmail.com>
2023-12-30 15:45:02 +0530

    Aboutus, Conactus modified

--- src/Components/Contactus.js
+++ src/Components/Contactus.js
@@ -1,9 +1,31 @@
-const Contactus = () =>{
+import "../Style/Aboutus.css";
+
+const Contactus = () => {
     return (
         <>
-            <h1>Contact us </h1>
-            <h3>On below phone </h3>
-            <h3>7020510901</h3>
+
+            <div className="contact">
+            <div className="contact-us">
+                <h1>Contact Us</h1>
+                <p>
+                    Have questions, suggestions, or just want to say hello? We'd love to hear from you!
+                    Reach out to us at <a href="mailto:info@eatsnow.com">info@eatsnow.com</a> or through the form below.
+                </p>
+
+                <form>
+                    <label htmlFor="name">Your Name:</label>
+                    <input type="text" id="name" name="name" required />
+
+                    <label htmlFor="email">Your Email:</label>
+                    <input type="email" id="email" name="email" required />
+
+                    <label htmlFor="message">Your Message:</label>
+                    <textarea id="message" name="message" rows="4" required></textarea>
+
+                    <button className="submitbtn" type="submit">Send Message</button>
+                </form>
+            </div>
+            </div>
         </>
     )
 }
